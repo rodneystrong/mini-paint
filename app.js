0bab40262664e1a3ac33a5b88577c4264f24cb0a
aleeex <akang2@gmail.com>
2016-07-07 11:45:50 -0600

    targeted box to change bg color to white

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+// all code goes in here
+// - Make it so that when we click on a pixel, it changes to white
+//   - Notice in the our index.html file each div has the class of 'box'
+//   - We can use this box class as our selector. This gives us a way to touch our DOM
+    var theBox = $('.box');
+    theBox.click(function() {
+      $(this).addClass('white');
+    });
+
+});
